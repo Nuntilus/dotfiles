114e831d7ce5a943ec9816d1e387bbf0cebc3752
Mio Galli <mio.galli@swisscom.com>
2026-05-28 08:06:03 +0200

    set nowrap as default

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,7 +2,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set nowrap")
 vim.g.mapleader = " "
+
 
 vim.opt.number = true
 vim.opt.relativenumber = true
