2f547f464bb8628ceb5b7565827dfd929a72cd82
Mio Galli <mio.galli@swisscom.com>
2026-05-29 11:28:31 +0200

    maped visual block mode to ctlr v

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -5,9 +5,11 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set nowrap")
 vim.g.mapleader = " "
 vim.nowrap = true
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
+
+-- Set ctlr-v to visual block mode 
+vim.keymap.set("n", "<C-v>", "<C-v>", {noremap = true, silent = true})
