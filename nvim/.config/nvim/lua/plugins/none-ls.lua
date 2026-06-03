085b2031f83c7c360106c3bdf47347ea014d4543
Your Name <153664386+Nuntilus@users.noreply.github.com>
2026-06-03 08:49:17 +0200

    fixed eslint configs to read from config files

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,12 +6,16 @@ return {
 	config = function()
 		local null_ls = require("null-ls")
 
+		-- Ensure Mason-installed binaries (e.g. eslint_d) are on PATH
+		vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin:" .. vim.env.PATH
+
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.rustfmt,
 				require("none-ls.diagnostics.eslint_d"),
+				require("none-ls.formatting.eslint_d"),
 			},
 		})
 	end,
